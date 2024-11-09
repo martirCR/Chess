116a0972531ec8b7d9c9df8bc90911140bb796e4
mikanilo <timothybailey690@gmail.com>
2024-11-09 15:37:10 -0600

    MovePiece

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -7,8 +7,8 @@ namespace Chess
     {
         private const int _boardLength = 8;
 
-        public Piece[,] ChessBoard { get; set; } = new Piece[_boardLength,_boardLength];
-        
+        public Piece[,] ChessBoard { get; set; } = new Piece[_boardLength, _boardLength];
+
         private const int _numberOfMoves = 32;
 
         private const int _pawn = 0;
@@ -22,7 +22,7 @@ namespace Chess
         private const int _queen = 4;
 
         private const int _king = 5;
-        
+
         private void SetSpecialPieces(bool IsWhite)
         {
             int row;
@@ -81,101 +81,124 @@ namespace Chess
 
 
         /// <summary>
-        /// Moves pece
+        /// if move is valid updates piece position and the chessboard index with said position
         /// </summary>
-        public void MovePiece(Piece p)
-
-        { }
-            public (int row, int col)[] ValidMove(Piece p)            {
+        public void MovePiece((int row, int col) move, (int row, int col)[] possible,Piece p)
+        {
+            if (IsValid(p))
+            {
                 
-                switch (p.Rank)
+                if(ChessBoard[move.row,move.col] == null)// if the space is empty
                 {
-                    case 0: // pawn
+                    p.Position = (move.row,move.col);
+                    ChessBoard[move.row, move.col] = p;
+                    return;
+                }
+                if(ChessBoard[move.row, move.col].IsWhite != p.IsWhite)//if it is a opposite color
+                {
+                    p.Position = (move.row, move.col);
+                    ChessBoard[move.row, move.col] = p;
+                    return;
+                }
+            }
+            return;
+        }
+        private bool MoveSearch ((int row, int col), (int row, int col)[] possible)
+        {
+            possible.
+        }
+
+        private (int row, int col)[] PossibleMoves(Piece p)
+        {
+
+            switch (p.Rank)
+            {
+                case 0: // pawn
                     {
                         (int row, int col)[] moves = new (int row, int col)[3];
-                            if (p.Start)
-                            {
-                                 moves[0] = (p.Position.row + 2, p.Position.col);
-                            }
-                            moves[1] = (p.Position.row + 1, p.Position.col);
-                            moves[2] = (p.Position.row + 1, p.Position.col - 1);
-                            moves[3] = (p.Position.row + 1, p.Position.col + 1);
-                            return moves;
-                        }
-                    case 1: // bishop
+                        if (p.Start)
                         {
-
-                            Queue<(int row, int col)> que = new Queue<(int row, int col)>();
-                            for (int i = 1; i < 8; i++)// 8 is the maximum number of spaces a piece can move
-                            {
-                                que.Enqueue((p.Position.row + i, p.Position.col + i));
-                                que.Enqueue((p.Position.row + i, p.Position.col - i));
-                                que.Enqueue((p.Position.row - i, p.Position.col + i));
-                                que.Enqueue((p.Position.row - i, p.Position.col - i));
-                            }
-                            (int row, int col)[] moves = new (int row, int col)[que.Count];
-                            for (int i = 0; i < que.Count; i++)
-                            {
-                                moves[i] = que.Dequeue();
-                            }
-                            return moves;
+                            moves[0] = (p.Position.row + 2, p.Position.col);
                         }
-                    case 2: // knight
+                        moves[1] = (p.Position.row + 1, p.Position.col);
+                        moves[2] = (p.Position.row + 1, p.Position.col - 1);
+                        moves[3] = (p.Position.row + 1, p.Position.col + 1);
+                        return moves;
+                    }
+                case 1: // bishop
+                    {
+
+                        Queue<(int row, int col)> que = new Queue<(int row, int col)>();
+                        for (int i = 1; i < 8; i++)// 8 is the maximum number of spaces a piece can move
                         {
-                            (int row, int col)[] moves = new (int row, int col)[7];
-                            moves[0] = (p.Position.row + 2, p.Position.col - 1);// tall up left
-                            moves[1] = (p.Position.row + 2, p.Position.col + 1);//tall up right
-                            moves[2] = (p.Position.row + 1, p.Position.col - 2); //short up left
-                            moves[3] = (p.Position.row + 1, p.Position.col + 2);//short up right
-                            moves[4] = (p.Position.row - 2, p.Position.col + 1);//tall down right
-                            moves[5] = (p.Position.row - 2, p.Position.col - 1);//tall down left
-                            moves[6] = (p.Position.row - 1, p.Position.col + 2);//short down right
-                            moves[7] = (p.Position.row - 1, p.Position.col - 2);//short down left
-                            return moves;
+                            que.Enqueue((p.Position.row + i, p.Position.col + i));
+                            que.Enqueue((p.Position.row + i, p.Position.col - i));
+                            que.Enqueue((p.Position.row - i, p.Position.col + i));
+                            que.Enqueue((p.Position.row - i, p.Position.col - i));
                         }
-                    case 3://rook
+                        (int row, int col)[] moves = new (int row, int col)[que.Count];
+                        for (int i = 0; i < que.Count; i++)
                         {
-                            Queue<(int row, int col)> que = new Queue<(int row, int col)>();
-                            for (int i = 1; i < 8; i++)// 8 is the maximum number of spaces a piece can move
-                            {
-                                que.Enqueue((p.Position.row + i, p.Position.col));
-                                que.Enqueue((p.Position.row - i, p.Position.col));
-                                que.Enqueue((p.Position.row, p.Position.col + i));
-                                que.Enqueue((p.Position.row, p.Position.col - i));
-                            }
-                            (int row, int col)[] moves = new (int row, int col)[que.Count];
-                            for (int i = 0; i < que.Count; i++)
-                            {
-                                moves[i] = que.Dequeue();
-                            }
-                            return moves;
+                            moves[i] = que.Dequeue();
                         }
-                    case 4://queen
+                        return moves;
+                    }
+                case 2: // knight
+                    {
+                        (int row, int col)[] moves = new (int row, int col)[7];
+                        moves[0] = (p.Position.row + 2, p.Position.col - 1);// tall up left
+                        moves[1] = (p.Position.row + 2, p.Position.col + 1);//tall up right
+                        moves[2] = (p.Position.row + 1, p.Position.col - 2); //short up left
+                        moves[3] = (p.Position.row + 1, p.Position.col + 2);//short up right
+                        moves[4] = (p.Position.row - 2, p.Position.col + 1);//tall down right
+                        moves[5] = (p.Position.row - 2, p.Position.col - 1);//tall down left
+                        moves[6] = (p.Position.row - 1, p.Position.col + 2);//short down right
+                        moves[7] = (p.Position.row - 1, p.Position.col - 2);//short down left
+                        return moves;
+                    }
+                case 3://rook
+                    {
+                        Queue<(int row, int col)> que = new Queue<(int row, int col)>();
+                        for (int i = 1; i < 8; i++)// 8 is the maximum number of spaces a piece can move
                         {
-                            Queue<(int row, int col)> que = new Queue<(int row, int col)>();
-                            for (int i = 1; i < 8; i++)// 8 is the maximum number of spaces a piece can move
-                            {
-                                que.Enqueue((p.Position.row + i, p.Position.col));
-                                que.Enqueue((p.Position.row - i, p.Position.col));
-                                que.Enqueue((p.Position.row, p.Position.col + i));
-                                que.Enqueue((p.Position.row, p.Position.col - i));
-                                que.Enqueue((p.Position.row + i, p.Position.col + i));
-                                que.Enqueue((p.Position.row + i, p.Position.col - i));
-                                que.Enqueue((p.Position.row - i, p.Position.col + i));
-                                que.Enqueue((p.Position.row - i, p.Position.col - i));
-                            }
-                            (int row, int col)[] moves = new (int row, int col)[que.Count];
-                            for (int i = 0; i < que.Count; i++)
-                            {
-                                moves[i] = que.Dequeue();
-                            }
-                            return moves;
-
-
-
-
+                            que.Enqueue((p.Position.row + i, p.Position.col));
+                            que.Enqueue((p.Position.row - i, p.Position.col));
+                            que.Enqueue((p.Position.row, p.Position.col + i));
+                            que.Enqueue((p.Position.row, p.Position.col - i));
                         }
-                    case 5://king
+                        (int row, int col)[] moves = new (int row, int col)[que.Count];
+                        for (int i = 0; i < que.Count; i++)
+                        {
+                            moves[i] = que.Dequeue();
+                        }
+                        return moves;
+                    }
+                case 4://queen
+                    {
+                        Queue<(int row, int col)> que = new Queue<(int row, int col)>();
+                        for (int i = 1; i < 8; i++)// 8 is the maximum number of spaces a piece can move
+                        {
+                            que.Enqueue((p.Position.row + i, p.Position.col));
+                            que.Enqueue((p.Position.row - i, p.Position.col));
+                            que.Enqueue((p.Position.row, p.Position.col + i));
+                            que.Enqueue((p.Position.row, p.Position.col - i));
+                            que.Enqueue((p.Position.row + i, p.Position.col + i));
+                            que.Enqueue((p.Position.row + i, p.Position.col - i));
+                            que.Enqueue((p.Position.row - i, p.Position.col + i));
+                            que.Enqueue((p.Position.row - i, p.Position.col - i));
+                        }
+                        (int row, int col)[] moves = new (int row, int col)[que.Count];
+                        for (int i = 0; i < que.Count; i++)
+                        {
+                            moves[i] = que.Dequeue();
+                        }
+                        return moves;
+
+
+
+
+                    }
+                case 5://king
                     {
                         Queue<(int row, int col)> que = new Queue<(int row, int col)>();
                         for (int i = 0; i < 1; i++)// 8 is the maximum number of spaces a piece can move
@@ -196,14 +219,14 @@ namespace Chess
                         }
                         return moves;
                     }
-                        
-                    
-                        
-                }
-            return null;
-            }
 
-        
+
+
+            }
+            return null;
+        }
+
+
 
 
         /// <summary>
@@ -215,6 +238,7 @@ namespace Chess
         {
             if (p.Position.row < _boardLength && p.Position.col < _boardLength)
             {
+               
                 if (p.IsWhite && ChessBoard[p.Position.row, p.Position.col] == p)
                 {
                     return false;
@@ -225,7 +249,7 @@ namespace Chess
                 }
 
                 return true;
-            } 
+            }
             else
             {
                 return false;
