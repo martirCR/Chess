3ca5e6a264a0999cb5064c02093ebc9e89c6abc6
mikanilo <timothybailey690@gmail.com>
2024-11-08 22:07:32 -0600

    Piece interface done

---
+++ Chess/Pawn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Pawn : Piece
+    {
+        public bool IsWhite { get; }
+
+        public (int row, int col) Position { get; set; }
+
+        public bool _start = true;
+        /// change number of slots in moes based on valid moves for piece, so pawn has 4 valid moves
+        public const int _numberOfMoves = 3;
+        //update in the board class to false once moved
+        public (int row, int col)[] ValidMove(Piece p)
+        {
+            
+            (int row, int col)[] moves = new (int row, int col)[_numberOfMoves];
+            if (_start)
+            {
+                moves[0] = (p.Position.row + 2,p.Position.col);
+            }
+            moves[1] = (p.Position.row + 1,p.Position.col);
+            moves[2] = (p.Position.row + 1, p.Position.col - 1);
+        }
+    }
+}
